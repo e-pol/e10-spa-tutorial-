dcc1a75a736abdce310488f0415feadb10aff110
e.pol <eugene.poletski@gmail.com>
2016-05-06 09:42:13 +0300

    Finished module first pass

--- spa/js/spa.model.js
+++ spa/js/spa.model.js
@@ -1,3 +1,17 @@
 /**
+ * spa.model.js
+ * Model module
  * Created by 123 on 06.05.2016.
  */
+
+/*jslint
+ browser : true,   continue : true,    devel : true,     indent   : 2,
+ maxerr  : 50,     newcap   : true,    nomen : true,     plusplus : true,
+ regexp  : true,   sloppy   : true,    vars : false,     white    : true
+ */
+/*global $, spa */
+
+spa.model = (function () {
+  "use strict";
+
+}());
